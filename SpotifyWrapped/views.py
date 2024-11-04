7554aab76d1eb05081b567a762ca08daa5e3f8d5
Flamebom <Flamebom@users.noreply.github.com>
2024-11-04 20:57:26 +0000

    autopep8 action fixes

--- SpotifyWrapped/views.py
+++ SpotifyWrapped/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
 # Renders
+
+
 def index(request):
     """Render the index page (login on startup)."""
     return render(request, '../UI/SpotifyUI/login.html', {})
+
 
 def profile(request):
     """Render the profile page (after login)."""
